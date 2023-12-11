b64d5e6e2b6b2903ef8c82177fdcd8744834c3e0
M92 <m92dev@mail.de>
2023-12-11 19:22:29 +0100

    add random computerChoice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+const choices = ["rock", "paper", "scissor"];
+
+function getComputerChoice() {
+  let choice = choices[Math.floor(Math.random() * choices.length)];
+  console.log(choice);
+}
